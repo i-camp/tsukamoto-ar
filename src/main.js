07760449df7a334b50892afc4f0bbdcee20d368b
kubotak-is <kubotak@istyle.co.jp>
2017-11-26 01:48:31 +0900

    add: use pubsub

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import Shooter from './Components/Shooter.js'
+import * as PubSub from 'pubsub-js'
+import EventType from './ValueObjects/EventType'
+
+PubSub.subscribe(EventType.shot, e => {
+  // TODO オブジェクトの中心判定
+  PubSub.publish(EventType.isHit, {name: "tsukamotota"});
+});
