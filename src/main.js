e451a41c48a46087d0cbef73c215c72405252ab4
kubotak-is <kubotak@istyle.co.jp>
2017-12-01 03:32:33 +0900

    modify: main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import Shooter from './Components/Shooter.js'
 import * as PubSub from 'pubsub-js'
 import EventType from './ValueObjects/EventType'
-import * as IndexAR from 'indexAR.js'
-
-PubSub.subscribe(EventType.shot, e => {
-  // TODO オブジェクトの中心判定
-  //PubSub.publish(EventType.isHit, {name: "tsukamotota"});
-});
+import * as IndexAR from './indexAR.js'
