b2258cea21eaa30cbd9da8ab171789acc0ef430d
okasuke <yokasuke@gmail.com>
2017-12-02 16:18:02 +0900

    commit忘れ

--- src/RestrictIOS.js
+++ src/RestrictIOS.js
@@ -3,12 +3,12 @@ window.addEventListener('touchmove', event => {
   event.preventDefault();
 });
 /* ピッチインピッチアウトによる拡大縮小を禁止 */
-document.documentElement.addEventListener('touchstart', function (e) {
+document.documentElement.addEventListener('touchstart', (e) => {
   if (e.touches.length >= 2) {e.preventDefault();}
 }, false);
 /* ダブルタップによる拡大を禁止 */
 let t = 0;
-document.documentElement.addEventListener('touchend', function (e) {
+document.documentElement.addEventListener('touchend', (e) => {
   let now = new Date().getTime();
   if ((now - t) < 350){
     e.preventDefault();
