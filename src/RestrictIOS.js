aa53858cd407e3547e45c19762ef42f75cb0af80
okasuke <yokasuke@gmail.com>
2017-12-02 15:48:47 +0900

    var宣言修正

--- src/RestrictIOS.js
+++ src/RestrictIOS.js
@@ -7,9 +7,9 @@ document.documentElement.addEventListener('touchstart', function (e) {
   if (e.touches.length >= 2) {e.preventDefault();}
 }, false);
 /* ダブルタップによる拡大を禁止 */
-var t = 0;
+let t = 0;
 document.documentElement.addEventListener('touchend', function (e) {
-  var now = new Date().getTime();
+  let now = new Date().getTime();
   if ((now - t) < 350){
     e.preventDefault();
   }
