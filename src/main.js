475d89933859fa08428fbf3c030039c64d018b9b
okasuke <yokasuke@gmail.com>
2017-12-02 17:43:53 +0900

    main.jsのisLoadedイベント削除

--- src/main.js
+++ src/main.js
@@ -4,8 +4,3 @@ import * as PubSub from 'pubsub-js'
 import EventType from './ValueObjects/EventType'
 import * as IndexAR from './indexAR.js'
 import * as RestrictIOS from './RestrictIOS.js'
-
-// sample is loaded
-//setTimeout(() => {
-  //PubSub.publish(EventType.isLoaded);
-//}, 5000);
