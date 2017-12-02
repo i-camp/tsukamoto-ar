e263c347e38730b60e6c5f74b28c4cd9b82a3d1a
kubotak-is <kubotak@istyle.co.jp>
2017-12-02 15:07:34 +0900

    add: loading

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+import Loading from './Components/Loading.js'
 import Shooter from './Components/Shooter.js'
 import * as PubSub from 'pubsub-js'
 import EventType from './ValueObjects/EventType'
 import * as IndexAR from './indexAR.js'
+
+
+// sample is loaded
+setTimeout(() => {
+  PubSub.publish(EventType.isLoaded);
+}, 5000);
